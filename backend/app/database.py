c33e86e8c1de14704f5ef2ff2cfcf151a3bafe00
GautierDERMONT <gautier.dermont1@gmail.com>
2026-06-12 20:45:35 +0200

    url data non visible

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL non définie dans le fichier .env")
 
 engine = create_engine(DATABASE_URL)
 
