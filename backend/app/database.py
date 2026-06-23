f591e5d6cca1d64dfa4e25f29205d5024049ae3a
mzunaid9 <zunaid.mougaidinessaib9@gmail.com>
2026-06-23 20:38:47 +0200

    modif pour deploiement render

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,11 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("❌ DATABASE_URL non définie dans le fichier .env")
+
+#Sécurité pour Render : SQLAlchemy veut "postgresql://" au lieu de "postgres://"
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
